605fa794a36d978733599cc34aae7cc7150c3add
Louise Bowler <LouiseABowler@users.noreply.github.com>
2020-02-19 17:04:22 +0000

    Get data from pdf into dataframe with correct columns

---
+++ payroll-to-excel.py
@@ -0,0 +1,71 @@
+import argparse
+import os
+import pandas as pd
+import pdftotext
+
+
+def parse_pdf_to_fwf(folder, file):
+
+    if file[:-4] != ".pdf":
+        Exception("Input file should have extension .pdf")
+
+    with open(os.path.join(folder, file), "rb") as in_pdf:
+        pdf = pdftotext.PDF(in_pdf)
+
+    text = pdf[0]
+    split_text = text.split("\n")
+
+    output_file = file[:-4] + ".txt"
+
+    with open(os.path.join(folder, output_file), "w") as outfile:
+        [outfile.write("%s\n" % l) for l in split_text[2:-1]]
+
+    return output_file
+
+
+def fwf_to_df(folder, file):
+
+    df = pd.read_fwf(os.path.join(folder, file), colspecs="infer")
+
+    return df
+
+
+def check_and_clean_df(df):
+
+    expected_columns = ["ELEMENT DETAILS", "BROUGHT FORWARD", "THIS PERIOD", "ADJUSTMENT", "CARRIED FORWARD"]
+
+    # Check columns
+
+    if len(list(df)) != len(expected_columns):
+        raise Exception("Number of columns is not {}, as expected".format(len(expected_columns)))
+
+    for col, expcol in zip(list(df), expected_columns):
+        if col != expcol:
+            raise Exception("Expected column name to be {}, not {}".format(expcol, col))
+
+    print(df)
+
+    # Check first column (should be three-parts, parse and separate, remove total row and retain for final check)
+
+    # Check second column (should be numbers with max 2 dp, change NaN -> 0, move ending - to start for negative)
+
+    # Check third column (should be numbers with max 2 dp, change NaN -> 0, move ending - to start for negative)
+
+    # Check fourth column (should be numbers with max 2 dp, change NaN -> 0, move ending - to start for negative)
+
+    # Check fifth column (should be numbers with max 2 dp, change NaN -> 0, move ending - to start for negative)
+
+    # Check total
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--folder", type=str, default=os.getcwd(), help="Path to directory containing pdf reports")
+    parser.add_argument("--first", type=str, help="Name of the file containing the first month's data")
+    args = parser.parse_args()
+
+    first_fwf = parse_pdf_to_fwf(args.folder, args.first)
+    first_df = fwf_to_df(args.folder, first_fwf)
+    first_df = check_and_clean_df(first_df)
+
